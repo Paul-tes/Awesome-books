59e0beee36fd115e454bfffd391e349e88551338
paul-tes <paulasca8462@gmail.com>
2023-04-25 08:28:56 +0300

    added UI class and addBookList method introduced

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ class Book {
     this.author = author;
   }
 }
+
+// ui class
+class UI {
+  static addBookList(book) {
+    const table = document.getElementById('book-list');
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+    <tr class="book-list-item">
+      <td class="title">${book.title} by ${book.author}</td>
+      <td class="remove-td-btn"><button class="remove-btn">remove</button></td>
+    </tr>
+    `;
+    table.appendChild(tr);
+  }
+}
