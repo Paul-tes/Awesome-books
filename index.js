55b8ceb2e5c3aadb335fea83b494173e47e7d9a5
paul-tes <paulasca8462@gmail.com>
2023-04-25 15:37:29 +0300

    Store class copied to in a new file Store

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import Store from "./Store";
+
 class Book {
   constructor(title, author) {
     this.title = title;
@@ -33,36 +35,6 @@ class UI {
   static clearInput() {
     document.querySelector('#title').value = '';
     document.querySelector('#author').value = '';
-  }
-}
-
-class Store {
-  static getBooks() {
-    let books;
-    if(localStorage.getItem('books') === null) {
-      books = [];
-    } else {
-      books = JSON.parse(localStorage.getItem('books'));
-    }
-
-    return books;
-  }
-
-  static addBook(book) {
-    let books = Store.getBooks();
-    books.push(book);
-    localStorage.setItem('books', JSON.stringify(books));
-  }
-
-  static removeBook(title) {
-    const books = Store.getBooks();
-    books.forEach((book, index) => {
-      if (book.title === title) {
-        books.splice(index, 1);
-      }
-    });
-
-    localStorage.setItem('books', JSON.stringify(books));
   }
 }
 
